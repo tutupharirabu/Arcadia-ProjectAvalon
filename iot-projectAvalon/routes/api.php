62c56ba4a861a12e1a5e4d651fd389c6618042cd
Irfan Zharauri Nanda Sudiyanto <zharalogic@student.telkomuniversity.ac.id>
2024-10-30 12:39:09 +0700

    Perbaikan api.php (lagi)

--- iot-projectAvalon/routes/api.php
+++ iot-projectAvalon/routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/api/status', function () {
+Route::get('/status', function () {
     $nodeServiceUrl = env('NODE_SERVICE_URL') . '/status';
 
     try {
