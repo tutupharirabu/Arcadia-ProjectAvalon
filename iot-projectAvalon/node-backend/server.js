2fb343a25f3123903f57f580808ce177471237c1
Irfan Zharauri Nanda Sudiyanto <zharalogic@student.telkomuniversity.ac.id>
2024-10-30 12:05:30 +0700

    Perbaiki server running

--- iot-projectAvalon/node-backend/server.js
+++ iot-projectAvalon/node-backend/server.js
@@ -65,5 +65,5 @@ app.get('/status', async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+    console.log(`Server running on https://localhost:${port}`);
 });
