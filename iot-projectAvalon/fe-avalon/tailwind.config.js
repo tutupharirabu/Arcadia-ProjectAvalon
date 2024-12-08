57b9dc4e1b463c18e06f5967fd420b665cab702b
DeazardMuhammad <deazard29@gmail.com>
2024-12-08 20:58:02 +0700

    membenarkan error

--- iot-projectAvalon/fe-avalon/tailwind.config.js
+++ iot-projectAvalon/fe-avalon/tailwind.config.js
@@ -4,19 +4,4 @@ module.exports = {
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        primary: {
-          DEFAULT: "#8b5cf6", // Warna utama
-        },
-        dark: {
-          DEFAULT: "#1c1b22",
-          lighter: "#2b2835",
-        },
-      },
-    },
-  },
-  plugins: [],
-};
-
+}
