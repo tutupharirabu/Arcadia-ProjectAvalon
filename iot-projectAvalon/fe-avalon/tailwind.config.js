2b47c0e927550dd7675de1eb2f2763ffc82a4154
DeazardMuhammad <deazard29@gmail.com>
2024-12-08 22:08:41 +0700

    bisa tampil

--- iot-projectAvalon/fe-avalon/tailwind.config.js
+++ iot-projectAvalon/fe-avalon/tailwind.config.js
@@ -1,7 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+// tailwind.config.js
+export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
