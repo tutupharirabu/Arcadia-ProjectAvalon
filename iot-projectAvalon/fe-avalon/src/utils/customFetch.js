b2bb84fe499afd41ce703fd0c75206e311ae2a1a
Irfan Zharauri Nanda Sudiyanto <zharalogic@student.telkomuniversity.ac.id>
2024-12-18 14:40:48 +0700

    FE - Perbaiki customfetch.js (Part 2)

--- iot-projectAvalon/fe-avalon/src/utils/customFetch.js
+++ iot-projectAvalon/fe-avalon/src/utils/customFetch.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const customFetch = axios.create({
-    baseURL: 'https://arcadia-beavalon-development.up.railway.app//api/v1'
+    baseURL: 'https://arcadia-beavalon-development.up.railway.app/api/v1'
   });
 
   export default customFetch
